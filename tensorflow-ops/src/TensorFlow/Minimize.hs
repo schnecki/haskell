3cd2e15e58e74b6a5de2bbf822a9650a741115c6
Manuel Schneckenreither <manuel.schneckenreither@uibk.ac.at>
2019-01-21 16:44:06 +0100

    Added Adam minimization helper functions for Tensor Ref's

--- tensorflow-ops/src/TensorFlow/Minimize.hs
+++ tensorflow-ops/src/TensorFlow/Minimize.hs
@@ -12,30 +12,42 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RankNTypes          #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeApplications    #-}
 
 module TensorFlow.Minimize
     ( Minimizer
+    , MinimizerRefs
     , minimizeWith
+    , minimizeWithRefs
     , gradientDescent
+    , gradientDescentRefs
     , AdamConfig(..)
     , adam
+    , adamRefs
     , adam'
+    , adamRefs'
     ) where
 
-import Control.Monad (zipWithM)
-import Data.Default (Default(..))
-import Data.List (zipWith4)
-import Data.Maybe (fromMaybe)
+import           Control.Monad          (zipWithM)
+import           Data.Default           (Default (..))
+import           Data.Int               (Int64)
+import           Data.List              (zipWith4)
+import           Data.Maybe             (fromMaybe)
 
-import qualified TensorFlow.Core as TF
-import qualified TensorFlow.Gradient as TF
-import qualified TensorFlow.Ops as TF hiding (assign, initializedVariable)
-import qualified TensorFlow.Variable as TF
+
+import qualified TensorFlow.Core        as TF
+import qualified TensorFlow.GenOps.Core as TFO (applyAdam, assignAdd, shape)
+import qualified TensorFlow.Gradient    as TF
+import qualified TensorFlow.Ops         as TF hiding (assign, initializedVariable)
+import qualified TensorFlow.Ops         as TFO (assign, initializedVariable,
+                                                initializedVariable', scalar,
+                                                zeroInitializedVariable, zeros)
+import qualified TensorFlow.Tensor      as TF (renderValue)
+import qualified TensorFlow.Variable    as TF
 
 -- | Functions that minimize a loss w.r.t. a set of 'TF.Variable's.
 --
@@ -47,6 +59,11 @@ type Minimizer a =
     forall m. TF.MonadBuild m =>
     [TF.Variable a] -> [TF.Tensor TF.Value a] -> m TF.ControlNode
 
+type MinimizerRefs a =
+    forall m. TF.MonadBuild m =>
+    [TF.Tensor TF.Ref a] -> [TF.Shape] -> [TF.Tensor TF.Value a] -> m (TF.ControlNode, [TF.Tensor TF.Ref a])
+
+
 -- | Convenience wrapper around 'TF.gradients' and a 'Minimizer'.
 minimizeWith :: (TF.MonadBuild m, TF.GradientCompatible a)
              => Minimizer a
@@ -56,6 +73,19 @@ minimizeWith :: (TF.MonadBuild m, TF.GradientCompatible a)
 minimizeWith minimizer loss params =
     TF.gradients loss params >>= minimizer params
 
+
+-- | Convenience wrapper around 'TF.gradients' and a 'Minimizer'.
+minimizeWithRefs :: (TF.MonadBuild m, TF.GradientCompatible a)
+             => MinimizerRefs a
+             -> TF.Tensor v a        -- ^ Loss.
+             -> [TF.Tensor TF.Ref a] -- ^ Parameters of the loss function.
+             -> [TF.Shape]           -- ^ Parameters shapes of the loss function.
+             -> m (TF.ControlNode, [TF.Tensor TF.Ref a])
+minimizeWithRefs minimizer loss params shapes = do
+    let vals = map TF.value params
+    TF.gradients loss vals >>= minimizer params shapes
+
+
 -- | Perform one step of the gradient descent algorithm.
 gradientDescent :: TF.GradientCompatible a
                 => a  -- ^ Learning rate.
@@ -64,6 +94,16 @@ gradientDescent learningRate params grads = TF.withNameScope "gradientDescent" $
     let applyGrad param grad =
             TF.assignAdd param (TF.scalar (-learningRate) `TF.mul` grad)
     TF.group =<< zipWithM applyGrad params grads
+
+-- | Perform one step of the gradient descent algorithm.
+gradientDescentRefs :: TF.GradientCompatible a
+                => a  -- ^ Learning rate.
+                -> MinimizerRefs a
+gradientDescentRefs learningRate params _ grads = TF.withNameScope "gradientDescent" $ do
+    let applyGrad param grad =
+            TFO.assignAdd param (TF.scalar (-learningRate) `TF.mul` grad)
+    gr <- TF.group =<< zipWithM applyGrad params grads
+    return (gr, [])
 
 -- TODO: Support more than Float in adam.
 
@@ -85,6 +125,10 @@ instance Default AdamConfig where
 -- NOTE: Currently requires all 'TF.Variable's to have an 'TF.initializedValue'.
 adam :: Minimizer Float
 adam = adam' def
+
+adamRefs :: MinimizerRefs Float
+adamRefs = adamRefs' def
+
 
 adam' :: AdamConfig -> Minimizer Float
 adam' config params grads = TF.withNameScope "adam" $ do
@@ -113,3 +157,31 @@ adam' config params grads = TF.withNameScope "adam" $ do
     updateBeta1 <- updateBeta beta1Power beta1
     updateBeta2 <- updateBeta beta2Power beta2
     TF.group (updateBeta1:updateBeta2:updateVars)
+
+
+adamRefs' :: AdamConfig -> MinimizerRefs Float
+adamRefs' config params shapes grads = TF.withNameScope "adamRefs" $ do
+    let lr = TF.scalar (adamLearningRate config)
+        beta1 = TF.scalar (adamBeta1 config)
+        beta2 = TF.scalar (adamBeta2 config)
+        epsilon = TF.scalar (adamEpsilon config)
+    -- Create adam state variables.
+    ms <- mapM TFO.zeroInitializedVariable shapes
+    vs <- mapM TFO.zeroInitializedVariable shapes
+    beta1Power <- TFO.initializedVariable beta1
+    beta2Power <- TFO.initializedVariable beta2
+    -- Perform adam update.
+    let applyGrad param m v =
+            TFO.applyAdam param m v beta1Power beta2Power
+                                 lr beta1 beta2 epsilon
+    updateVars <- sequence $ zipWith4 applyGrad params ms vs grads
+    -- Update beta variables after adam update.
+    let updateBeta betaPower beta =
+            TF.withControlDependencies updateVars
+                (TFO.assign betaPower (betaPower `TF.mul` beta))
+    updateBeta1 <- updateBeta beta1Power beta1
+    updateBeta2 <- updateBeta beta2Power beta2
+    grp <- TF.group (updateBeta1:updateBeta2:updateVars)
+    let vars = [beta1Power, beta2Power, updateBeta1, updateBeta2] ++ updateVars
+    return (grp, vars)
+
